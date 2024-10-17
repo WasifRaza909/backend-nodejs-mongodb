b258045e3c0736d21db8412b0ceb858d9621c89f
wasif-technyx <142510796+wasif-technyx@users.noreply.github.com>
2024-10-17 10:45:15 +0500

    Create basic routes

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+require('dotenv').config( )
 const express = require('express')
 const app = express()
-const port = 3000
+
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+  res.send('wasifraza2002')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Please login at</h1>')
+})
+app.get('/youtube', (req, res) => {
+  res.send('<h2>Youtube</h2>')
 })
 
 app.listen(port, () => {
